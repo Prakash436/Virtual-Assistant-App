8764c013938d7a0d066f853975982d6ea15d85d8
Prakash436 <prakashkumar43674@gmail.com>
2023-06-28 00:42:30 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import plus from "../src/icons/icons8-plus-24.png";
+import send from "../src/icons/icons8-send-24.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="side-bar">
+        <button className="new-chat">
+        <img src={plus} className="plus-icon" alt="+"></img>
+        New chat</button>
+        <ul className="history">
+          <li>Hey</li>
+          <li>Bro</li>
+        </ul>
+      </div>
+      <div className="main">
+        <h1>ChatGPT</h1>
+        <ul className="feed">
+
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <img id="submit" src={send} alt="->"></img>
+          </div>
+          <p className="info">
+            Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 24 Version
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
